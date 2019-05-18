218bf782d7b25a4d35740ac73fc98084f04c2b40
Jonathan Morales <Jonathan@Jonathans-MacBook-Pro.local>
2019-05-18 15:31:01 -0400

    Adding animation index

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,27 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+  myFunction();
+  pivotIndex();
+};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+const header = document.getElementById("myHeader");
+const sticky = header.offsetTop;
 
-function myFunction() {
+const myFunction = () => {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
+  }
+}
+
+const halfwayPoint = document.getElementById("indexTitle");
+const halfwayPointMeasurement = halfwayPoint.offsetTop;
+const image = document.querySelector(".inner-index-right-image");
+const text = document.querySelector(".inner-first");
+
+const pivotIndex = () => {
+  if (window.pageYOffset > halfwayPointMeasurement) {
+    image.classList.add("rotate-left");
+    text.classList.add("rotate-right");
   }
 }
